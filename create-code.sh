fda217f39362f3d8008b3d279806fb5d560217c9
mxklb <mxklb@protonmail.com>
2020-07-30 17:20:52 +0200

    Corrected version in ag call to match VERSION

--- create-code.sh
+++ create-code.sh
@@ -1,3 +1,3 @@
 #!/bin/ash
-ag /tmp/python-paho-template/api/$1 @asyncapi/python-paho-template --force-write
+ag /tmp/python-paho-template/api/$1 @asyncapi/python-paho-template@$2 --force-write
 chown 1000:1000 *
